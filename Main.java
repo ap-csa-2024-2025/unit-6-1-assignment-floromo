56da72c7f6d67eb042293a6b301b480697afdf5e
floromo <145050916+floromo@users.noreply.github.com>
2025-02-03 18:14:50 +0000

    donezo

--- Main.java
+++ Main.java
@@ -5,17 +5,40 @@ public class Main
   public static void main(String[] args)
   {
     // Problem 1 - Create a scanner and an array of size 3
+    Scanner sc = new Scanner(System.in);
+    
+    double[] arr = new double[3];
+    System.out.println("Type 3 numbers: ");
+    int i = 0, sum = 0;
+    while (i < arr.length){
+      arr[i] = sc.nextDouble();
+      sum += arr[i];
+      i++;
+    }
+    System.out.println("Your numbers are: " + arr[0] + ", " + arr[1] + ", " + arr[2] + "\n" + "Your sum is: " + sum);
+
 
     // Problem 2
     // Use the scanner to get user input and give it to the method
-
-    int N = /* scan.nextInt() */;
+    int N = sc.nextInt();
     int[] output = sumsOfNums(N);
+    i = 0;
+    while (i < output.length){
+      System.out.print(output[i] + " ");
+      i++;
+    }
   }
 
   public static int[] sumsOfNums(int input)
   {
-    // Implement Problem 2 here
-    return 0;
+    int[] numbers = new int[input];
+    for (int i = 0; i < input; i++){
+      int n = 0;
+      for (int j = i; j > 0; j--){
+        n += j;
+      }
+      numbers[i] = n;
+    }
+    return numbers;
   }
 }
